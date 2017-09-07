c546591cb3911d4728f4dcb2ad1435221937bf07
smtrjn <sumit.rajan@gmail.com>
2017-09-07 20:01:55 +0530

    Changes up to mailer object

---
+++ routes/surveyRoutes.js
@@ -0,0 +1,25 @@
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+const mongoose = require('mongoose');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+
+const Survey = mongoose.model('surveys');
+
+module.exports = app => {
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      date: Date.now()
+    });
+
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    mailer.send();
+  });
+};
